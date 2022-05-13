d6087c7976859a04fae098a4ffe9127e22ea0bfc
Andrew Goetz <agoetz4407@gmail.com>
2022-05-12 22:58:50 -0500

    working on getting front end to display saved notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,4 +5,20 @@ router.get('/notes', (req, res) => {
     res.json(notes)
 })
 
+// router.post('/notes', (req, res) => {
+//     req.body.id = animals.length.toString()
+    
+//     if (!validateAnimal(req.body)) {
+//         res.status(400).send('The animal sent is not properly formatted')
+//     } else {
+//         const animal = createNewAnimal(req.body, animals)
+//         res.json(animal)
+//     }
+// })
+
+//TODO: add delete route
+// router.delete('/notes/:id', (req, res) => {
+
+// })
+
 module.exports = router;
