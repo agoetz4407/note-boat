a046787bfa0a1a4230076254794acc82c10f5d68
Andrew Goetz <agoetz4407@gmail.com>
2022-05-14 15:04:46 -0500

    get and post route working

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
 const fs = require('fs');
+const path = require('path');
 const notes = require('../../db/db.json')
 
 router.get('/notes', (req, res) => { 
-    res.json(notes)
+    res.json(notes);
+})
+
+router.post('/notes', (req, res) => {
+    const notesArr = notes;
+    const newId = notesArr[notesArr.length - 1].id + 1;
+    const newNote = req.body;
+    newNote.id = newId;
+    notesArr.push(newNote)
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(notesArr)
+    )
+    res.json({
+        message:'Note saved!',
+        savedNote: newNote
+    })
 })
 
 module.exports = router;
