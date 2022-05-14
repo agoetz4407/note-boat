d1ae2d4f630f68733299a47732a682fb6defaff8
Andrew Goetz <agoetz4407@gmail.com>
2022-05-14 14:14:32 -0500

    got get route working

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,24 +1,12 @@
 const router = require('express').Router();
-const notes = require('../../db/db.json')
+const fs = require('fs');
+let notes;
 
 router.get('/notes', (req, res) => {
-    res.json(notes)
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
+        notes = JSON.parse(data);
+    });
+    res.json(notes.savedNotes)
 })
-
-// router.post('/notes', (req, res) => {
-//     req.body.id = animals.length.toString()
-    
-//     if (!validateAnimal(req.body)) {
-//         res.status(400).send('The animal sent is not properly formatted')
-//     } else {
-//         const animal = createNewAnimal(req.body, animals)
-//         res.json(animal)
-//     }
-// })
-
-//TODO: add delete route
-// router.delete('/notes/:id', (req, res) => {
-
-// })
 
 module.exports = router;
