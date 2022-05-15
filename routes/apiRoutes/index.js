14c9feb1042fb9e3704208c638345f272a7bf347
Andrew Goetz <agoetz4407@gmail.com>
2022-05-15 17:06:05 -0500

    bugged delete branch

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,21 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-const notes = require('../../db/db.json')
+const uniqid = require('uniqid'); 
+// const notes = require('../../db/db.json')
+let notes;
 
 router.get('/notes', (req, res) => { 
-    res.json(notes);
+    fs.readFile('../../db/db.json', 'utf-8', (err, data) => {
+        notes = data;
+    })
+    res.json(notes)
 })
 
 router.post('/notes', (req, res) => {
     const notesArr = notes;
-    const newId = notesArr[notesArr.length - 1].id + 1;
     const newNote = req.body;
-    newNote.id = newId;
+    newNote.id = uniqid();
     notesArr.push(newNote)
     fs.writeFileSync(
         path.join(__dirname, '../../db/db.json'),
@@ -20,6 +24,24 @@ router.post('/notes', (req, res) => {
     res.json({
         message:'Note saved!',
         savedNote: newNote
+    })
+})
+
+router.delete('/notes/:id', (req, res) => {
+    const notesArr = notes;
+    const noteId = req.params.id;
+    const newNotesArr = notesArr.filter(note => {
+        if (note.id != noteId) {
+            return note
+        }
+    });
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(newNotesArr)
+    )
+    res.json({
+        message:'Note deleted!',
     })
 })
 
